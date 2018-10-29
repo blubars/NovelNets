433debb130b8ff433edb1101988f130e4c7fdde6
Carl Mueller <cmuell89@gmail.com>
2018-10-29 13:47:09 -0600

    BS update

--- src/ner.py
+++ src/ner.py
@@ -1,7 +1,8 @@
 import spacy
 
-nlp = spacy.load('en_core_web_sm')
-doc = nlp(u'Apple is looking at buying U.K. startup for $1 billion')
+nlp = spacy.load('en_core_web_md')
+doc = nlp(u"displaCy uses JavaScript, SVG and CSS.")
+spacy.displacy.serve(doc, style='dep')
 
 for ent in doc.ents:
     print(ent.text, ent.start_char, ent.end_char, ent.label_)
