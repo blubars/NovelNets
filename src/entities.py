a19bb3c2649261e96259717ac5904ed6c07df5c6
Hunter Wapman <hunter.wapman@gmail.com>
2018-12-01 19:39:54 -0700

    ENTITIES

--- src/entities.py
+++ src/entities.py
@@ -92,43 +92,40 @@ def process_section(section_num):
             handle_missed_entity(missing_entity)
 
 
-def handle_missed_entity(missing_entity):
+def handle_missed_entity(psuedonym):
     entities = get_saved_entities()
     entity_names = sorted(list(entities.keys()))
     for i, entity in enumerate(entity_names):
         print(f"- {i}\t{entity}")
 
-    print(f"if {missing_entity} is a psuedonym of someone in the list, enter their number. Else 'n' or enter.")
+    print(f"if {psuedonym} is a psuedonym of someone in the list, enter their number. Else an id to use.")
     answer = input()
 
-    if answer == 'n' or not len(answer):
-        handle_create_new_entity(missing_entity)
+    prompt_string = ""
+    if answer.isalpha():
+        entity = answer
+        prompt_string = f"add entity [{entity}] with psuedonym [{psuedonym}]? ('n' to skip)"
     else:
-        entity_index = int(answer)
-        psuedonym = missing_entity
-        handle_create_entity_pseudonym(entity_names[entity_index], psuedonym)
+        entity = entity_names[int(answer)]
+        prompt_string = f"add to entity [{entity}] psuedomyn [{psuedonym}]? ('n' to skip)"
     
-
-def handle_create_new_entity(psuedonym):
-    print("input an entity id for this entity psuedonym:")
-    entity = input()
-    print(f"add entity [{entity}] with psuedonym [{psuedonym}]? ('n' to skip)")
+    print(prompt_string)
     answer = input()
     if answer != 'n':
         add_entity_psuedonym(entity, psuedonym)
 
-def handle_create_entity_pseudonym(entity, psuedonym):
-    print(f"add to entity [{entity}] psuedomyn [{psuedonym}]? ('n' to skip)")
-    answer = input()
-    if answer != 'n':
-        add_entity_psuedonym(entity, psuedonym)
 
 if __name__ == '__main__':
     sections = [i for i in range(1, 193)]
-    processed_sections = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37]
+    processed_sections = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 192]
 
     # section 21 is POV (Hal)
     # section 36 we're missing 2 ingersolls for possibly a hyphen?
+    # remove 'John' from john wayne
+    # 89 will require close reading. (which I haven't done)
+    # ditto on 94
+
+    # fackelman -->  fackelmann
 
     sections_to_process = [i for i in sections if i not in processed_sections]
 
