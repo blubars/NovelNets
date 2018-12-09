f0d3011df15f7839c5b5b8bc54ce35cbb284873a
Hunter Wapman <hunter.wapman@gmail.com>
2018-12-08 19:33:32 -0700

    changed webweb to have chronological and normal

--- src/display_graph.py
+++ src/display_graph.py
@@ -12,8 +12,6 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    section_sequence = get_section_sequence(args.chronological)
-
     gg = Graphify(300, 50)
     gg.load(SAVE_GRAPH_PATH, range(1,193))
 
@@ -22,8 +20,16 @@ if __name__ == '__main__':
     web.display.sizeBy = 'degree'
     web.display.l = 60
     web.display.c = 120
+    web.display.w = 600
+    web.display.h = 600
+
+    section_sequence = get_section_sequence(chronological=False)
 
     for G in gg.graph_by_sections(section_sequence, aggregate=True):
         web.networks.infinite_jest.add_frame_from_networkx_graph(G)
+
+    section_sequence = get_section_sequence(chronological=True)
+    for G in gg.graph_by_sections(section_sequence, aggregate=True):
+        web.networks.infinite_jest_chronological.add_frame_from_networkx_graph(G)
 
     web.draw()
