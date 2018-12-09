70ad53b32fce2187f8d9257b1d172d263d350002
blubars <blubars@gmail.com>
2018-12-09 01:20:22 -0700

    analyze attachment

--- src/analyze.py
+++ src/analyze.py
@@ -14,7 +14,12 @@
 # Imports
 #########################################################
 import json
+import os
+import csv
+import pandas as pd
 import networkx as nx
+import matplotlib
+import matplotlib.pyplot as plt
 
 from graphify import Graphify
 from utils import get_sections_path
@@ -23,11 +28,27 @@ from utils import get_sections_path
 # Globals
 #########################################################
 SAVE_GRAPH_PATH = '../data/graph/'
+ANALYSIS_PATH = '../data/analysis/'
 TOTAL_NUM_SECTIONS = 192
 
 #########################################################
 # Function definitions
 #########################################################
+def plot_df(df, x_name, y_name, title, logx=False):
+    fig = plt.figure(1)
+    X = df[x_name]
+    Y = df[y_name]
+    plt.plot(X, Y)
+    plt.title(title)
+    plt.ylabel(y_name)
+    plt.xlabel(x_name)
+    if logx:
+        plt.xscale('log')
+    plt.tight_layout()
+    fname = x_name.lower() + '_vs_' + y_name.lower() + ".pdf"
+    fname = fname.replace(" ", "_")
+    plt.savefig(ANALYSIS_PATH + fname)
+    plt.close(fig)
 
 def get_chronological_order():
     # read json files to extract chronological sect order
@@ -64,7 +85,7 @@ def get_chronological_order():
 def analyze_centralities(G):
     # centrality measures
     centralities = [
-        (nx.degree_centrality, "Degree"), 
+        (nx.degree_centrality, "Degree"),
         (nx.betweenness_centrality, "Betweenness"),
         (nx.eigenvector_centrality, "Eigenvector")
     ]
@@ -92,16 +113,58 @@ def graphify_whole_book(chronological=False, load_from_file=False):
         gg.process_book(sections)
     return gg
 
-def analyze_dynamics(gg, seq):
+def analyze_attachment(gg, weighted=True):
+    # does degree distribution follow a power law?
+    # compare degree distribution to price's model
+    # calculate 'age' of node in terms of section it first appears in
+    # calculate ccdf of degree distribution
+    if weighted:
+        ks = sorted((k for (node,k) in gg.G.degree(weight='weight')), reverse=True)
+    else:
+        ks = sorted((k for (node,k) in gg.G.degree()), reverse=True)
+    #for section in range(0, len(ks)+1, 2):
+    #for G in gg.graph_by_sections(seq, aggregate=True):
+    fig = plt.figure(1)
+    bins = [k+1 for k in range(max(ks)+1)]
+    cumbins = [0 for k in range(max(ks)+1)]
+    for k in ks:
+        cumbins[k] += 1
+    print("Nodes with 0 citations: {}%".format(cumbins[k]/len(cumbins)))
+    for i in range(len(cumbins)-2, -1, -1):
+        cumbins[i] = (cumbins[i+1] + cumbins[i])
+    for i in range(len(cumbins)):
+        cumbins[i] /= len(cumbins)
+    plt.loglog(bins, cumbins)
+    plt.title("Degree Distribution CCDF")
+    plt.ylabel("Compl. Cumulative Distr Function")
+    plt.xlabel("Degree, $k$")
+    plt.legend()
+    plt.tight_layout()
+    fname = "degree_distr_ccdf.pdf"
+    plt.savefig(ANALYSIS_PATH + fname)
+
+def analyze_dynamics(gg, seq, weighted=True):
+    # geodesic_vs_degree
+    out_csv_name = ANALYSIS_PATH + 'geodesic_vs_degree.csv'
+    if not os.path.isdir(ANALYSIS_PATH):
+        os.makedirs(ANALYSIS_PATH)
+    try:
+        csvf = open(out_csv_name, 'w', newline='')
+        writer = csv.writer(csvf)
+    except:
+        print("Failed to create csv file, quitting")
+        return
+    writer.writerow(["Section", "n", "avg degree", "avg geodesic len", "num components", "largest component size"])
+
     # for each section, calculate avg degree & mean geodesic
     for i,G in enumerate(gg.graph_by_sections(seq, aggregate=True)):
         num_components = 1
+        largest_component = (-1,0)
         try:
             avg_len = nx.average_shortest_path_length(G, weight="weight")
         except nx.NetworkXError:
             avg_len = 0
             num_components = 0
-            largest_component = (-1,0)
             for ci,C in enumerate(nx.connected_component_subgraphs(G)):
                 ni = nx.number_of_nodes(C)
                 if ni > largest_component[1]:
@@ -109,10 +172,25 @@ def analyze_dynamics(gg, seq):
                     largest_component = (ci, ni)
                 num_components += 1
 
-        degs = [k for (node,k) in G.degree()]
+        if weighted:
+            degs = [k for (node,k) in G.degree(weight='weight')]
+        else:
+            degs = [k for (node,k) in G.degree()]
         avg_degree = sum(degs) / len(degs)
         n = nx.number_of_nodes(G)
-        print("Section:{}\tn:{}\n  avg deg:{}, avg geodesic:{}, num_components={}".format(seq[i], n, avg_degree, avg_len, num_components))
+        largest_component_size = n if num_components == 1 else largest_component[1]
+        print("Section:{}\tn:{}\n  avg deg:{}, avg geodesic:{}, num_components={}"
+            .format(seq[i], n, avg_degree, avg_len, num_components))
+        writer.writerow([seq[i], n, avg_degree, avg_len, num_components, largest_component_size])
+    csvf.close()
+    # plot results.
+    df = pd.read_csv(out_csv_name)
+    plot_df(df, "avg degree", "avg geodesic len")
+
+def make_plots():
+    df = pd.read_csv(ANALYSIS_PATH + 'geodesic_vs_degree.csv')
+    plot_df(df, "avg degree", "avg geodesic len", "Attachment: Degree vs Avg Geodesic Path, By Section")
+    plot_df(df, "n", "avg geodesic len", "Attachment: Log(n) vs Avg Geodesic Path", logx=True)
 
 if __name__ == "__main__":
     print("Creating graphs")
@@ -124,8 +202,10 @@ if __name__ == "__main__":
     gg = graphify_whole_book(load_from_file=True)
 
     print("Analyzing book!")
-    analyze_dynamics(gg, get_section_sequence())
-    analyze_centralities(gg.G)
-    gg.web.draw()
+    #analyze_dynamics(gg, get_section_sequence())
+    #analyze_centralities(gg.G)
+    analyze_attachment(gg, weighted=False)
+    #make_plots()
+    #gg.web.draw()
 
 
